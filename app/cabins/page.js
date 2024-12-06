041f4cf4c4f142d2d81cba6df19304b3cae557f2
dasith jayanandana <dasith.jayanandana@velou.com>
2024-12-06 08:55:11 +0530

    434-sytling-with-tailwind-css

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,7 @@
-import Counter from "@/starter/components/Counter";
-
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-  console.log(data);
-
+export default function Page() {
   return (
     <div>
       <h1>Cabin page</h1>
-      <ul>{data.map(user => <li>{user.name}</li>)}</ul>
-      <Counter users={data}/>
     </div>
   );
 }
