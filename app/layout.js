5b36280adf15b0438cdf5288dba2ebdc5969bae4
dasith jayanandana <dasith.jayanandana@velou.com>
2024-12-20 15:36:57 +0530

    437-Improving the Navigation and Root Layout

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,36 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
-import {Josefin_Sans} from "next/font/google"
-import "@/app/_styles/globals.css"
+// 1.Import the name form google
+import { Josefin_Sans } from "next/font/google";
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
+// 2.Configure it
 const josafin = Josefin_Sans({
   subsets: ["latin"],
-  display: "swap"
-})
+  display: "swap",
+});
 
 export const metadata = {
   // title: "The Wild Oasis",
   title: {
     template: "%s / The Wild Oasis",
-    default: "Welcome / The Wild Oasis"
+    default: "Welcome / The Wild Oasis",
   },
-  description: "Luxurious cabin hotel"
+  description: "Luxurious cabin hotel",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body className={`${josafin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>copyright by @wild-oasis</footer>
+      // 3.Import the className
+      <body
+        className={`${josafin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto bg-red-600">{children}</main>
+        </div>
       </body>
     </html>
   );
