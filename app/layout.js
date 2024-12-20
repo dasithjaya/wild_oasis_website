d3d7deaf5a0de0a2c6a772106ac4ed06228bc9c4
dasith jayanandana <dasith.jayanandana@velou.com>
2024-12-20 15:22:40 +0530

    436-Loading and Optimizing Fonts

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,26 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import {Josefin_Sans} from "next/font/google"
 import "@/app/_styles/globals.css"
+
+const josafin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap"
+})
 
 export const metadata = {
   // title: "The Wild Oasis",
   title: {
     template: "%s / The Wild Oasis",
     default: "Welcome / The Wild Oasis"
-  }
+  },
+  description: "Luxurious cabin hotel"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body className={`${josafin.className} bg-primary-950 text-primary-100 min-h-screen`}>
         <header>
           <Logo />
           <Navigation />
